346babbd2b1949b944bf82002963076222a7be9c
LeLoader <leloaderpro@gmail.com>
2024-12-02 18:01:11 +0100

    Basic main

--- GuAcAmOlE/GuAcAmOlE.cpp
+++ GuAcAmOlE/GuAcAmOlE.cpp
@@ -3,10 +3,19 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "wtypes.h"
+
+using namespace std;
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
+    sf::CircleShape circle; // Déclaration de "circle" (sans affectation)
+    circle.setFillColor(sf::Color::Red);
+    circle.setRadius(100.0f); // Appel de méthode qui travaille sur "circle"
+
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "AvocadoRush", sf::Style::Fullscreen);
+
     // Initialise everything below
     // Game loop
     while (window.isOpen()) {
@@ -18,21 +27,10 @@ int main()
             }
         }
         window.clear();
-        sf::CircleShape circle; // Déclaration de "circle" (sans affectation)
-        circle.setFillColor(sf::Color::Red);
-        circle.setRadius(100.0f); // Appel de méthode qui travaille sur "circle"
-        // Whatever I want to draw goes here
+
+        //Dessine à chaque frame
+        window.draw(circle);
+
         window.display();
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
