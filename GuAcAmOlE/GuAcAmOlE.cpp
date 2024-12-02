d7ce6cace0670cac8e9198e34e8bee9d19cc8e9c
LeLoader <leloaderpro@gmail.com>
2024-12-02 16:50:18 +0100

    Base SFML

--- GuAcAmOlE/GuAcAmOlE.cpp
+++ GuAcAmOlE/GuAcAmOlE.cpp
@@ -1,11 +1,28 @@
 // GuAcAmOlE.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
+    // Initialise everything below
+    // Game loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Process any input event here
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        sf::CircleShape circle; // Déclaration de "circle" (sans affectation)
+        circle.setRadius(100.0f); // Appel de méthode qui travaille sur "circle"
+        // Whatever I want to draw goes here
+        window.display();
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
