35a9cd07927fd1c5917f5886b0c3865371cf97d6
Margot <margotdelage02@gmail.com>
2024-12-09 17:58:13 +0100

    block

--- GuAcAmOlE/Main.h
+++ GuAcAmOlE/Main.h
@@ -14,9 +14,15 @@ namespace Main
 	void InitAllObjects();
 	//extern float timeSpawn;
 	//
-	//extern std::vector<float> posObt = { 0.0f, 300.0f, 600.0f };
+	extern std::vector<float> posObt;
 	//extern std::vector<Object*> listObt;
+
+	extern float widthEcran;
+
 
 	//extern void spawnObt(float deltaTime, float timeSpawn, std::vector<float> posObt, std::vector<Object*> listObt);
 	extern void spawnObt(float deltaTime);
+	extern void DeleteObt();
+
+	extern void DeleteAllObt();
 };
