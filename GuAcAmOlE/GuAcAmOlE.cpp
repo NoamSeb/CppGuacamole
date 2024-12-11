f48ab1feb38c792296d250399497686ed75c127e
LeLoader <leloaderpro@gmail.com>
2024-12-11 16:20:43 +0100

    Merge

--- GuAcAmOlE/GuAcAmOlE.cpp
+++ GuAcAmOlE/GuAcAmOlE.cpp
@@ -14,7 +14,7 @@ int main()
 	//sf::RenderWindow window(sf::VideoMode(800, 600), "AvocadoRush");
 	Main::posObt = { 0.0f, (float)window.getSize().y / 2, (float)window.getSize().y - 120 };
 	Main::widthEcran = (float)window.getSize().x;
-    Main::heightEcran = (float)window.getSize().y;
+	Main::heightEcran = (float)window.getSize().y;
 
 	sf::Clock clock;
 
@@ -29,159 +29,79 @@ int main()
 	//Timer
 	float timeElapsed = 0;
 
-    //Restart
-    //bool gameOver = true;
+	//Restart
+	//bool gameOver = true;
 
-    while (window.isOpen()) 
-    {
-        float deltaTime = clock.getElapsedTime().asSeconds();
-        timeElapsed += deltaTime;
-        clock.restart();
+	while (window.isOpen())
+	{
+		float deltaTime = clock.getElapsedTime().asSeconds();
+		timeElapsed += deltaTime;
+		clock.restart();
 
-        sf::Event event;
-        while (window.pollEvent(event)) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
 
-            // Process any input event here
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-                if (event.type == sf::Event::KeyPressed)
-                {
-                    if (event.key.code == sf::Keyboard::Key::Space)
-                    {
-                        Ennemy* object = new Ennemy(Object::ShapeType::Rectangle, true);
-                    }
-
-                    if (event.key.code == sf::Keyboard::Key::R) //Restart
-                    {
-                        Main::DeleteAllBlocks();
-                        std::cout << "Init Game" << std::endl;
-                        timeElapsed = 0;
-                        deltaTime = 0;
-                        _HUD->InitGame();
-                        //Main::EnnemySpawner();
-                    }
-                }
-                if (event.type == sf::Event::KeyPressed)
-                {
-                    player.processEvents(event.key.code, true);
-                }
-                if (event.type == sf::Event::KeyReleased)
-                {
-                    player.processEvents(event.key.code, false);
-                }
-        }
-
-        // COLLISION
-
-        for (ICollider* colliderA : Main::CollidingObjects) {
-            for (ICollider* colliderB : Main::CollidingObjects) {
-               if (colliderA != colliderB) {
-                   if (colliderA->collisionShape->getGlobalBounds().intersects(colliderB->collisionShape->getGlobalBounds())) { // Both rectangle intersects
-                       colliderA->OnTriggerEnter(colliderB);
-                       // dynamic_cast<ICollider*>(b)->OnTriggerEnter(a); can be used if we skipped next iteration on same object
-                   }
-               }
-            }
-        }
-
-        // LOGIC
-
-        Main::spawnObt(deltaTime);
-
-        for (auto objectToTick : Main::Objects) {
-            if (objectToTick->bTick) {
-                objectToTick->Tick(deltaTime);
-            }
-        }
-
-        window.clear();
-
-        // RENDER
-
-        for (auto objectToDraw : Main::Objects)
-        {
-            window.draw(*(objectToDraw->shape));
-        }
-
-        sf::Text myTimer = _HUD->CreateTimerText(timeElapsed);
-        window.draw(myTimer);
-
-		while (window.isOpen())
-		{
-			float deltaTime = clock.getElapsedTime().asSeconds();
-			timeElapsed += deltaTime;
-			clock.restart();
-
-			sf::Event event;
-			while (window.pollEvent(event)) {
-
-				// Process any input event here
-				if (event.type == sf::Event::Closed) {
-					window.close();
-				}
-				if (event.type == sf::Event::KeyPressed)
-				{
-					if (event.key.code == sf::Keyboard::Key::Space)
-					{
-						Object* object = new Object(Object::ShapeType::Circle, true);
-					}
-
-					if (event.key.code == sf::Keyboard::Key::B)
-					{
-						if (Main::Objects.size() > 0)
-							Main::Objects.front()->Destroy();
-					}
-					if (event.key.code == sf::Keyboard::Key::R) //Restart
-					{
-						std::cout << "Init Game" << std::endl;
-						timeElapsed = 0;
-						deltaTime = 0;
-						_HUD->InitGame();
-					}
-
-					if (event.key.code == sf::Keyboard::Key::L) //Restart
-					{
-						window.close();
-					}
-				}
-				if (event.type == sf::Event::KeyPressed)
-				{
-					player.processEvents(event.key.code, true);
-				}
-				if (event.type == sf::Event::KeyReleased)
-				{
-					player.processEvents(event.key.code, false);
-				}
-				//}     
-
+			// Process any input event here
+			if (event.type == sf::Event::Closed) {
+				window.close();
 			}
-
-			Main::spawnObt(deltaTime);
-
-			// LOGIC    
-
-			for (auto objectToTick : Main::Objects) {
-				if (objectToTick->bTick) {
-					objectToTick->Tick(deltaTime);
-				}
-			}
-
-			window.clear();
-			;
-
-			// RENDER
-
-			for (auto objectToDraw : Main::Objects)
+			if (event.type == sf::Event::KeyPressed)
 			{
-				window.draw(*(objectToDraw->shape));
+				if (event.key.code == sf::Keyboard::Key::R) //Restart
+				{
+					Main::DeleteAllBlocks();
+					std::cout << "Init Game" << std::endl;
+					timeElapsed = 0;
+					deltaTime = 0;
+					_HUD->InitGame();
+					//Main::EnnemySpawner();
+				}
 			}
-
-			sf::Text myTimer = _HUD->CreateTimerText(timeElapsed);
-			window.draw(myTimer);
-
-
-			window.display();
+			if (event.type == sf::Event::KeyPressed)
+			{
+				player.processEvents(event.key.code, true);
+			}
+			if (event.type == sf::Event::KeyReleased)
+			{
+				player.processEvents(event.key.code, false);
+			}
 		}
+
+		// COLLISION
+
+		for (ICollider* colliderA : Main::CollidingObjects) {
+			for (ICollider* colliderB : Main::CollidingObjects) {
+				if (colliderA != colliderB) {
+					if (colliderA->collisionShape->getGlobalBounds().intersects(colliderB->collisionShape->getGlobalBounds())) { // Both rectangle intersects
+						colliderA->OnTriggerEnter(colliderB);
+						// dynamic_cast<ICollider*>(b)->OnTriggerEnter(a); can be used if we skipped next iteration on same object
+					}
+				}
+			}
+		}
+
+		// LOGIC
+
+		Main::spawnObt(deltaTime);
+
+		for (auto objectToTick : Main::Objects) {
+			if (objectToTick->bTick) {
+				objectToTick->Tick(deltaTime);
+			}
+		}
+
+		window.clear();
+
+		// RENDER
+
+		for (auto objectToDraw : Main::Objects)
+		{
+			window.draw(*(objectToDraw->shape));
+		}
+
+		sf::Text myTimer = _HUD->CreateTimerText(timeElapsed);
+		window.draw(myTimer);
+
+		window.display();
 	}
 }
